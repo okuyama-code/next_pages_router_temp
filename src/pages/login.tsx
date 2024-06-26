776126e459f07eb3ec30932e95be3b84ee9160fd
okuyama-code <ooyy0121@gmail.com>
2024-06-26 16:52:32 +0900

    toastをカスタマイズする

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,21 +1,24 @@
 import { Box, Button, useNotice } from '@yamada-ui/react'
+import { Bird } from "@yamada-ui/lucide"
+
 import React from 'react'
 
 const login = () => {
-  const succuesNotice = useNotice({
+  const successNotice = useNotice({
     component: ({ description }) => (
-      <Box color="white" py={3} px={4} bg="green.400" rounded="md">
+      <Box color="white" py={3} px={4} bg="green.400" rounded="md" display="flex" alignItems="center">
+        <Bird color="black" mr={2}/> 
         {description}
       </Box>
     ),
     duration: 1500,
     status: "success",
     variant: "subtle",
-  })
+  });
 
   const handleSubmit = () => {
     console.log("handleSubmit!!")
-    succuesNotice({ description: "ログインしました" })
+    successNotice({ description: "ログインしました" })
   }
 
   return (
