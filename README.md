d6ad17cc0ec1876e91e992fb5c8914be409f88d5
okuyama-code <ooyy0121@gmail.com>
2024-06-29 07:14:57 +0900

    認証の動作確認完了

--- README.md
+++ README.md
@@ -6,10 +6,22 @@ npm i
 ```
 touch .env.local
 ```
-.env.localに記述
+.env.localに記述 (Github, Google認証のセットアップあり)
 ```
 # portは各自変える
 NEXT_PUBLIC_API_BASE="http://localhost:8080"
+
+# next-authのGithub認証
+# https://qiita.com/y_inoue15/items/d6942cd6e71ff3169822
+GITHUB_ID=Ov23li9Bjfkmyd7L9fEV
+GITHUB_SECRET=a12b1cdf7d98e7f62d08619454197e0cc090407c
+# ランダムな文字列
+SECRET=gavivahwkhgwusisbwwihwwso
+
+# next-authのGoogle認証
+# https://zenn.dev/hayato94087/articles/91179fbbe1cad4
+GOOGLE_ID=7159146072-vot4k35259jtqfpcik8ikcb7ig9el16n.apps.googleusercontent.com
+GOOGLE_SECRET=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
 ```
 
 ## 起動
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,53 +11,73 @@ const Header = () => {
   const textColor = useColorModeValue("gray.800", "white");
 
   return (
-    <Box as="header" bg={bgColor} px={4} py={2} shadow="md">
-      <Flex alignItems="center" justifyContent="space-between">
-        <Link href="/" passHref>
-          <Text fontSize="xl" fontWeight="bold" color={textColor}>
-            個人開発
-          </Text>
-        </Link>
+    <div>
+      {/* header */}
+      <Box as="header" bg={bgColor} px={4} py={2} shadow="md">
+        <Flex alignItems="center" justifyContent="space-between">
+          <Link href="/" passHref>
+            <Text fontSize="xl" fontWeight="bold" color={textColor}>
+              個人開発
+            </Text>
+          </Link>
 
-        <Flex alignItems="center">
-          <IconButton
-            icon={useColorModeValue(<Moon />, <Sun />)}
-            onClick={toggleColorMode}
-            variant="ghost"
-            aria-label="Toggle color mode"
-            mr={2}
-          />
+          <Flex alignItems="center">
+            <IconButton
+              icon={useColorModeValue(<Moon />, <Sun />)}
+              onClick={toggleColorMode}
+              variant="ghost"
+              aria-label="Toggle color mode"
+              mr={2}
+            />
 
-          {session ? (
-            <Menu>
-              <MenuButton as={Button} rightIcon={<User />}>
-                {session.user?.name || session.user?.email}
-              </MenuButton>
-              <MenuList>
-                <MenuItem onClick={() => signOut()}>Sign out</MenuItem>
-              </MenuList>
-            </Menu>
-          ) : (
-            <Flex>
-              <Button onClick={() => signIn()} mr={2}>
-                Sign in
-              </Button>
-              <Link href="/register" passHref>
-                <Button colorScheme="primary">Register</Button>
-              </Link>
-            </Flex>
-          )}
+            {session ? (
+              <Menu>
+                <MenuButton as={Button} rightIcon={<User />}>
+                  {session.user?.name || session.user?.email}
+                </MenuButton>
+                <MenuList>
+                  <MenuItem onClick={() => signOut()}>Sign out</MenuItem>
+                </MenuList>
+              </Menu>
+            ) : (
+              <Flex>
+                <Button onClick={() => signIn()} mr={2}>
+                  Sign in
+                </Button>
+                <Link href="/register" passHref>
+                  <Button colorScheme="primary">Register</Button>
+                </Link>
+              </Flex>
+            )}
 
-          <IconButton
-            display={{ base: 'flex', md: 'none' }}
-            icon={<MenuIcon />}
-            variant="ghost"
-            aria-label="Open menu"
-            ml={2}
-          />
+            <IconButton
+              display={{ base: 'flex', md: 'none' }}
+              icon={<MenuIcon />}
+              variant="ghost"
+              aria-label="Open menu"
+              ml={2}
+            />
+          </Flex>
         </Flex>
-      </Flex>
-    </Box>
+      </Box>
+
+      {session && (
+        <>
+          name:{session?.user?.name} <br />
+          image:
+          {session?.user?.image && (
+            <img
+              src={session.user.image}
+              alt="icon"
+              style={{ width: "100px", height: "100px" }}
+            />
+          )}
+          <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
+
+    </div>
   );
 };
 
